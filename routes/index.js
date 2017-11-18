d5cfd6a1ff835f370b48970a82405c5931876baf
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2017-11-18 16:33:08 +0530

    add to cart

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var Cart = require('../models/cart');
 
 var csrf = require('csurf');
 var router = express.Router();
@@ -17,6 +18,22 @@ router.get('/', function(req, res, next) {
             productChunks.push(docs.slice(i, i + chunkSize));
         }
         res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+    });
+});
+
+// add products to cart
+router.get('/add-to-cart/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    Product.findById(productId, function (err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
     });
 });
 
